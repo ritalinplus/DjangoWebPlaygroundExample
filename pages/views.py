0cd762e6d0f73216600dfecd47406d2484b72cd5
Javier Abella <abeljvrg@gmail.com>
2020-06-12 11:32:02 +0200

    Add first version of pages app

---
+++ pages/views.py
@@ -0,0 +1,49 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from pages.models import Page
+from pages.forms import PageForm
+
+
+class StaffRequiredMixin(object):
+    """This mixin required a staff user."""
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class PagesListView(ListView):
+    model = Page
+    # paginate_by = 100
+
+
+class PageDetailsView(DetailView):
+    model = Page
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(CreateView):
+    model = Page
+    form_class = PageForm
+    success_url = reverse_lazy('pages:pages')
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class PageUpdate(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
